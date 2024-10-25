7f18c03988ebf3c01541ef01612df5c987dbeb38
Hepheir <hepheir@gmail.com>
2024-10-26 06:59:40 +0900

    feat(lift): remove retrival endpoints (GET /ev/camera/, GET /ev/people-count/)

--- app/lift/views.py
+++ app/lift/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import parsers
 from rest_framework import permissions
@@ -7,18 +6,12 @@ from . import models
 from . import serializers
 
 
-class CameraFrameAPIView(generics.CreateAPIView, generics.RetrieveAPIView):
+class CameraFrameAPIView(generics.CreateAPIView):
     parser_classes = [parsers.MultiPartParser]
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.CameraFrameSerializer
 
-    def get_object(self):
-        return get_object_or_404(models.Camera, auth=self.request.user).get_latest_frame_or_404()
 
-
-class PeopleCountAPIView(generics.CreateAPIView, generics.RetrieveAPIView):
+class PeopleCountAPIView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.CameraPeopleCountSerializer
-
-    def get_object(self):
-        return get_object_or_404(models.Camera, auth=self.request.user).get_latest_people_count_or_404()
